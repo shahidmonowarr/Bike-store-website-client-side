115670567b131faf2923d81ec452ece319332559
shahidmonowarr <shahidmonowar21@gmail.com>
2023-02-23 12:54:14 +0600

    updated

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,20 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import Rating from 'react-rating';
-import customer from '../../images/customer.png';
+import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import Rating from "react-rating";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import customer from "../../images/customer.png";
 
 const Review = () => {
-    const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-        fetch('https://bike-store-website-server.onrender.com/reviews')
-            .then(res => res.json())
-            .then(data => setReviews(data))
-    }, []);
-    return (
-        <div className="container">
-            <h2 className="title">CUSTOMER REVIEWS</h2>
+  useEffect(() => {
+    fetch("https://bike-store-website-server.onrender.com/reviews")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
 
-            <div className="row text-center">
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="container">
+      <h2 className="title">CUSTOMER REVIEWS</h2>
+
+      {reviews.length === 0 ? (
+        <div className="">
+          <h2>
+            <Spinner className="mt-5" animation="border" variant="warning" />
+          </h2>
+          <h3 className="text-warning">Loading...</h3>
+        </div>
+      ) : (
+        <Slider {...settings}>
+          {reviews.map((review) => (
+            <div
+              key={review._id}
+              style={{
+                background: "linear-gradient(90deg, #4b6cb7 0%, #182848 100%)",
+                color: "white",
+                width: "50%",
+                margin: "auto",
+                padding: "15px",
+              }}
+              className="mb-5"
+            >
+              <div
+                style={{
+                  background:
+                    "linear-gradient(90deg, #4b6cb7 0%, #182848 100%)",
+                  color: "white",
+                  width: "50%",
+                  height: "35vh",
+                  margin: "auto",
+                  padding: "15px",
+                }}
+                className=""
+              >
+                <img
+                  style={{ width: "10%" }}
+                  className="img-fluid mx-auto pb-3"
+                  src={customer}
+                  alt=""
+                />
+                <h2>{review.displayName}</h2>
+                <p>{review.reviewText}</p>
+                <Rating
+                  style={{ color: "orange" }}
+                  initialRating={review.rating}
+                  emptySymbol="far fa-star"
+                  fullSymbol="fas fa-star"
+                  readonly
+                />
+              </div>
+            </div>
+          ))}
+        </Slider>
+      )}
+
+      {/* <div className="row text-center">
                 {
                     reviews.map(review => <div key={review._id} className="col-md-4 mb-5">
                         <div className="border py-5">
@@ -31,9 +98,9 @@ const Review = () => {
                         </div>
                     </div>)
                 }
-            </div>
-        </div>
-    );
+            </div> */}
+    </div>
+  );
 };
 
 export default Review;
